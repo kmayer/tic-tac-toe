0eb145ba6594cdcb2be86a9fd01a579702c12a1d
Ken Mayer <ken@bitwrangler.com>
2014-01-02 08:26:30 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TicTacToe::Application.config.secret_key_base = '0d99a74367a0b21a37de53df6648856d4185b6f71b6b8136b181887c53bef32aefefcba7c46c851dbc047aa92c054cc3f2d38f77ee21a793f17d59358aeb1277'
