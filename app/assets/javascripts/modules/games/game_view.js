c2357ef5bd63c44bdceeeaa52b59b4fc5273daec
Ken Mayer <ken@bitwrangler.com>
2014-01-04 21:59:27 -0800

    GameView triggers 'take:turn'

--- app/assets/javascripts/modules/games/game_view.js
+++ app/assets/javascripts/modules/games/game_view.js
@@ -6,11 +6,19 @@ app.module("GamesModule", function(thisModule, thisApp) {
       "change" : "render"
     },
 
+    events: {
+      "click td[data-position]" : "cellClicked"
+    },
+
     serializeData: function() {
       return _.inject(this.model.get('board').split(''), function(memo, position, index) {
-        memo["pos"+index] = position;
+        memo["pos"+index] = (position == "X" || position == "O") ? position : "&nbsp;";
         return memo;
       }, {})
+    },
+
+    cellClicked: function(event) {
+      this.trigger("take:turn", $(event.currentTarget).data('position'));
     }
   });
 });
--- spec/javascripts/modules/games/game_view_spec.js
+++ spec/javascripts/modules/games/game_view_spec.js
@@ -7,4 +7,16 @@ describe("GamesModule.GameView", function() {
   it("#serializeData", function() {
     expect(JSON.stringify(this.view.serializeData())).toBe(JSON.stringify({pos0: "0", pos1: "1", pos2: "2", pos3: "3", pos4: "4", pos5: "5", pos6: "6", pos7: "7", pos8: "8"}));
   });
+
+  it("triggers 'take:turn'", function() {
+    var spy = jasmine.createSpy('click');
+
+    this.view.on("take:turn", spy)
+
+    this.view.render();
+
+    this.view.$("td[data-position=0]").click();
+
+    expect(spy).toHaveBeenCalledWith(0);
+  });
 });
